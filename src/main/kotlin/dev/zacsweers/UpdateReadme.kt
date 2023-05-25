7acdbbf0c00aa37313e6e18f5719674f3c7fd97f
Zac Sweers <pandanomic@gmail.com>
2023-05-25 19:31:13 -0400

    Reuse retryWithExponentialBackoff

--- src/main/kotlin/dev/zacsweers/UpdateReadme.kt
+++ src/main/kotlin/dev/zacsweers/UpdateReadme.kt
@@ -7,25 +7,22 @@ import com.github.ajalt.clikt.parameters.types.file
 import com.slack.eithernet.ApiResult
 import com.slack.eithernet.ApiResult.Failure.HttpFailure
 import com.slack.eithernet.ApiResult.Success
+import com.slack.eithernet.retryWithExponentialBackoff
 import com.squareup.moshi.Moshi
 import com.tickaroo.tikxml.TikXml
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
-import okhttp3.OkHttpClient
-import retrofit2.HttpException
 import java.time.Instant
 import java.time.ZoneId
 import kotlin.system.exitProcess
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import retrofit2.HttpException
 
 class UpdateReadmeCommand : CliktCommand() {
 
-  val outputFile by option("-o", help = "The README.md file to write")
-    .file()
-    .required()
+  val outputFile by option("-o", help = "The README.md file to write").file().required()
 
   override fun run() {
-    val okHttpClient = OkHttpClient.Builder()
-      .build()
+    val okHttpClient = OkHttpClient.Builder().build()
 
     val githubActivity = fetchGithubActivity(okHttpClient)
     val blogActivity = fetchBlogActivity(okHttpClient)
@@ -38,72 +35,45 @@ class UpdateReadmeCommand : CliktCommand() {
   }
 }
 
-private fun fetchBlogActivity(
-  client: OkHttpClient
-): List<ActivityItem> {
-  val blogApi = BlogApi.create(
-    client, TikXml.Builder()
-      .exceptionOnUnreadXml(false)
-      .addTypeConverter(Instant::class.java, InstantTypeConverter())
-      .build()
-  )
+private fun failureLogger(failure: ApiResult.Failure<*>) {
+  if (failure is HttpFailure<*>) {
+    val error = failure.error
+    check(error is HttpException?)
+    System.err.println("HttpException: $error\n${error?.response()?.errorBody()?.string()}")
+  }
+}
+
+private fun fetchBlogActivity(client: OkHttpClient): List<ActivityItem> {
+  val blogApi =
+    BlogApi.create(
+      client,
+      TikXml.Builder()
+        .exceptionOnUnreadXml(false)
+        .addTypeConverter(Instant::class.java, InstantTypeConverter())
+        .build()
+    )
 
   return runBlocking {
-    val result = retryWithBackoff(5) {
-      blogApi.main()
-    }
+    val result =
+      retryWithExponentialBackoff(maxAttempts = 5, onFailure = ::failureLogger) { blogApi.main() }
     when (result) {
       is Success -> {
-        result.value.channel
-          .itemList
+        result.value.channel.itemList
           .map { entry ->
-            ActivityItem(
-              text = "[${entry.title}](${entry.link})",
-              timestamp = entry.pubDate
-            )
+            ActivityItem(text = "[${entry.title}](${entry.link})", timestamp = entry.pubDate)
           }
           .take(10)
       }
-
       else -> error("Could not load blog content: $result")
     }
   }
 }
 
-private suspend fun <T : Any, E : Any> retryWithBackoff(
-  times: Int,
-  initialDelay: Long = 1000, // 1 second
-  maxDelay: Long = 60000,    // 60 seconds
-  factor: Double = 2.0,
-  block: suspend () -> ApiResult<T, E>
-): ApiResult<T, E> {
-  var currentDelay = initialDelay
-  repeat(times - 1) {
-    val result = block()
-    if (result is Success) {
-      return result
-    } else {
-      if (result is HttpFailure) {
-        val error = result.error
-        check(error is HttpException?)
-        System.err.println("HttpException: $error\n${error?.response()?.errorBody()?.string()}")
-      }
-    }
-    System.err.println("Retry #${it + 1}, delaying $currentDelay")
-    delay(currentDelay)
-    currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelay)
-  }
-  return block() // last attempt
-}
-
-
-private fun fetchGithubActivity(
-  client: OkHttpClient
-): List<ActivityItem> {
+private fun fetchGithubActivity(client: OkHttpClient): List<ActivityItem> {
   val moshi = Moshi.Builder().build()
   val githubApi = GitHubApi.create(client, moshi)
   val result = runBlocking {
-    retryWithBackoff(10) {
+    retryWithExponentialBackoff(maxAttempts = 10, onFailure = ::failureLogger) {
       githubApi.getUserActivity("ZacSweers")
     }
   }
@@ -113,21 +83,20 @@ private fun fetchGithubActivity(
         .filter { it.public }
         .mapNotNull { event ->
           when (val payload = event.payload) {
-            UnknownPayload, null -> return@mapNotNull null
+            UnknownPayload,
+            null -> return@mapNotNull null
             is IssuesEventPayload -> {
               ActivityItem(
                 "${payload.action} issue [#${payload.issue.number}](${payload.issue.htmlUrl}) on ${event.repo?.markdownUrl()}: \"${payload.issue.title}\"",
                 event.createdAt
               )
             }
-
             is IssueCommentEventPayload -> {
               ActivityItem(
                 "commented on [#${payload.issue.number}](${payload.comment.htmlUrl}) in ${event.repo?.markdownUrl()}",
                 event.createdAt
               )
             }
-
             is PullRequestPayload -> {
               val action = if (payload.pullRequest.merged == true) "merged" else payload.action
               ActivityItem(
@@ -135,14 +104,12 @@ private fun fetchGithubActivity(
                 event.createdAt
               )
             }
-
             is CreateEvent -> {
               ActivityItem(
                 "created ${payload.refType}${payload.ref?.let { " `$it`" } ?: ""} on ${event.repo?.markdownUrl()}",
                 event.createdAt
               )
             }
-
             is DeleteEvent -> {
               ActivityItem(
                 "deleted ${payload.refType}${payload.ref?.let { " `$it`" } ?: ""} on ${event.repo?.markdownUrl()}",
@@ -153,7 +120,6 @@ private fun fetchGithubActivity(
         }
         .take(10)
     }
-
     else -> error("Could not load GitHub activity.")
   }
 }
@@ -162,10 +128,7 @@ fun main(argv: Array<String>) {
   UpdateReadmeCommand().main(argv)
 }
 
-data class ActivityItem(
-  val text: String,
-  val timestamp: Instant
-) {
+data class ActivityItem(val text: String, val timestamp: Instant) {
   override fun toString(): String {
     return "**${timestamp.atZone(ZoneId.of("America/New_York")).toLocalDate()}** — $text"
   }
